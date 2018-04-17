f1dec38439de11ce6ffaf831ec7a8b21edddb242
James Zaki <james.zaki@gmail.com>
2018-04-17 17:41:59 +1000

    Ticket purchase functionality

--- random.sol
+++ random.sol
@@ -1,5 +1,34 @@
 contract Random {
 
+    address private host;
+    address[] public players;
+    uint constant public TICKET_PRICE = 0.01 ether;
+    bool public closed = false;
+    bytes32 public hashedSeed;
+    uint public blockNumberToUse;
 
+    function Random() public {
+        host = msg.sender;
+        players.push(host);
+    }
+    
+    modifier onlyHost() {
+        require(msg.sender == host);
+        _;
+    }
 
+    function () public payable {
+        require(msg.value == 0);
+    }
+
+    function purchaseTicket() public payable returns (address[]) {
+        require(closed == false);
+        require(msg.value > TICKET_PRICE);
+        uint256 refund = msg.value - TICKET_PRICE;
+        if (refund > 0) {
+            msg.sender.transfer(refund);
+        }
+        players.push(msg.sender);
+        return players;
+    }
 }
