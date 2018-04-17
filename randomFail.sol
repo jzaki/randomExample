eca2ce57af9cb8db5a646221676104fc449111c7
James Zaki <james.zaki@gmail.com>
2018-04-17 16:10:30 +1000

    added bad random implementations for determining winner

--- randomFail.sol
+++ randomFail.sol
@@ -29,7 +29,31 @@ contract RandomFail {
         players.push(msg.sender);
         return players;
     }
-
+    
+    function determineWinnerTime_NO() public onlyHost returns (address winner) {
+        uint random = uint(keccak256(now));
+        uint winnerIndex = random % players.length;
+        winner = players[winnerIndex];
+        players.length = 0;
+        winner.transfer(address(this).balance);
     }
 
+    function determineWinnerHash_NO() public onlyHost returns (address winner) {
+        bytes32 previousBlockHash = block.blockhash(block.number-1);
+        uint random = uint(keccak256(previousBlockHash));
+        uint winnerIndex = random % players.length;
+        winner = players[winnerIndex];
+        players.length = 0;
+        winner.transfer(address(this).balance);
+    }
+
+    function determineWinnerSeedHash_NO(uint randomSeed) public onlyHost returns (address winner) {
+        bytes32 previousBlockHash = block.blockhash(block.number-1);
+        uint random = uint(keccak256(randomSeed + uint(previousBlockHash)));
+        uint winnerIndex = random % players.length;
+        winner = players[winnerIndex];
+        players.length = 0;
+        winner.transfer(address(this).balance);
+    }
+    
 }
